d15cb0c252733e0ac0bd19e9550f9f96134e3f6b
yissel <contrerasjared0610@gmail.com>
2020-03-23 19:19:32 -0500

    lates changes on fronEnd

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Kids from "./kidsfrontEnd.html/kidsfrontEnd";
+
+
+
 
 function App() {
   return (
@@ -10,6 +14,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Kids/>
         <a
           className="App-link"
           href="https://reactjs.org"
