5ffb2842005592cf8404341fe37b2f4c283d9ced
yasmin yazdi <yyazdi13@gmail.com>
2020-05-06 18:55:35 -0500

    deploy

--- server.js
+++ server.js
@@ -41,9 +41,9 @@ controller(app);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
-  app.get("*", function(req, res){
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  })
+  // app.get("*", function(req, res){
+  //   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  // })
 }
 
 app.listen(PORT, function() {
